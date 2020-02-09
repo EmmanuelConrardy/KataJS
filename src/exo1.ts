6a208a2b82f1e69720f50e09759b21946e508b84
emmanuel conrardy <emmanuel.conrardy@arolla.fr>
2020-02-09 14:50:34 +0100

    start-exo1

---
+++ src/exo1.ts
@@ -0,0 +1,26 @@
+//Exercice 1
+//Print 12345
+
+var result = '';
+p1;
+export async function print() {
+  await f();
+  result += '5';
+  return result;
+}
+
+async function f() {
+ await p2
+  result += await print34();
+}
+
+async function print34() {
+  await p3
+  return await p4;
+}
+
+var p4 = new Promise(resolver => resolver(4))
+var p1 = new Promise(resolver => resolver(result += '1'))
+var p3 = new Promise(resolver => resolver(result += '3'))
+var p2 = new Promise(resolver => resolver(result += '2'))
+
